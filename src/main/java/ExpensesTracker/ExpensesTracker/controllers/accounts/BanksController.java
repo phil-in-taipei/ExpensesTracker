e2626952548aeac893507c36a1679f66ad2f97f4
mikalikahi <sweeney.phil@gmail.com>
2023-03-26 16:19:30 +0800

    Attempted htmx form submission for creating/displaying banks. Didn't understand how to bind form object in Thymeleaf to htmx post

--- src/main/java/ExpensesTracker/ExpensesTracker/controllers/accounts/BanksController.java
+++ src/main/java/ExpensesTracker/ExpensesTracker/controllers/accounts/BanksController.java
@@ -21,12 +21,39 @@ public class BanksController {
         return "accounts/banks";
     }
 
+    @GetMapping("/banks-htmx")
+    public String banksHtmx(Model model) {
+        Bank bank = new Bank();
+        model.addAttribute("bank", bank);
+        List<Bank> banks = bankService.getAllBanks();
+        model.addAttribute("banks", banks);
+        return "accounts/banks-htmx";
+    }
+
     @GetMapping("/create-bank")
     public String showSubmitBankPage(Model model) {
         Bank bank = new Bank();
         model.addAttribute("bank", bank);
         return "accounts/create-bank";
     }
+
+    @PostMapping(path = "/create-bank-htmx")
+    public String create(@RequestParam("bank") Bank bank, Model model) {
+        model.addAttribute("bank", bank);
+        System.out.println("Now Submitting bank via htmx");
+        System.out.println(bank);
+        try {
+            bankService.saveBank(bank);
+        } catch (IllegalArgumentException e) {
+            model.addAttribute(
+                    "message",
+                    "Could not save bank, "
+                            + e.getMessage());
+            return "error/error";
+        }
+        return "banks-htmx :: banks-htmx";
+    }
+
 
     @PostMapping("/submit-bank")
     public String saveNewBank(
