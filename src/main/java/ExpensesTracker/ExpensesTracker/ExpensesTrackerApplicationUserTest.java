4a602e54953f0c0f2593b575feea9968b2d5d0cb
mikalikahi <sweeney.phil@gmail.com>
2023-06-04 09:47:28 +0800

    Explanatory comments for bootstrapper class and profile used just for testing UserDetailsServiceImp class

--- src/main/java/ExpensesTracker/ExpensesTracker/ExpensesTrackerApplicationUserTest.java
+++ src/main/java/ExpensesTracker/ExpensesTracker/ExpensesTrackerApplicationUserTest.java
@@ -1,13 +1,17 @@
 package ExpensesTracker.ExpensesTracker;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 
+// this version of the command line runner exists to run unit tests of the
+// UserDetailsServiceImp. This is because the UserRepo used in the
+// ExpensesTrackerApplicationTest command line runner clashes with the Mocked UserRepo
+// in the UserDetailsServiceImpUnitTest class
+
 @SpringBootApplication
 @Profile("test")
-public class ExpensesTrackerApplicationUserTest { //implements CommandLineRunner
+public class ExpensesTrackerApplicationUserTest {
 
     public static void main(String[] args) {
         SpringApplication
