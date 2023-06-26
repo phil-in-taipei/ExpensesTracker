3be5dfac0ecafaa760062da09808e7be3a60468e
mikalikahi <sweeney.phil@gmail.com>
2023-06-26 15:54:40 +0800

    More endpoint tests for methods in DepositsController

--- src/test/java/ExpensesTracker/ExpensesTracker/controllerEndpoints/transactions/DepositsControllerEndpointTests.java
+++ src/test/java/ExpensesTracker/ExpensesTracker/controllerEndpoints/transactions/DepositsControllerEndpointTests.java
@@ -26,16 +26,18 @@ import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest(classes = ExpensesTrackerApplication.class)
 @ActiveProfiles("test")
@@ -86,6 +88,153 @@ public class DepositsControllerEndpointTests {
                 .andExpect(redirectedUrl("/user-deposits-current-month"));
         savingAccountsRepo.delete(testSavingsAccount);
         incomeSourceRepo.delete(testIncomeSource);
+    }
+
+    @Test
+    @Order(4)
+    @WithUserDetails("Test Expenses Manager User1")
+    public void testSearchDepositsByMonthAndYear()
+            throws Exception {
+        MockHttpServletRequestBuilder submitSearch =
+                post("/search-deposits-by-month-year")
+                        .param("month", String.valueOf(month))
+                        .param("year", "2023");
+        mockMvc.perform(submitSearch)
+                //.andDo(print())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentType("text/html;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeExists("deposits"))
+                .andExpect(model().attributeExists("year"))
+                .andExpect(model().attributeExists("month"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Date")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Account")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Amount")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Source")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Income Source 1")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Bank Account 1")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("200.0")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Expenses Manager User1")))
+                .andExpect(view().name(
+                        "transactions/user-deposits-by-month.html"));
+    }
+
+    @Test
+    @Order(2)
+    @WithUserDetails("Test Expenses Manager User1")
+    public void testSearchDepositsByMonthAndYearPage() throws Exception {
+        mockMvc
+                .perform(get("/search-deposits-by-month-and-year"))
+                //.andDo(print())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentType("text/html;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeExists("monthOptions"))
+                .andExpect(model().attributeExists("searchMonthAndYear"))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("2023")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("2023")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("JANUARY")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("FEBRUARY")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("MARCH")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("APRIL")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("MAY")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("JUNE")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("JULY")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("AUGUST")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("SEPTEMBER")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("OCTOBER")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("NOVEMBER")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("DECEMBER")))
+                .andExpect(view().name(
+                        "transactions/search-deposits-by-month-and-year.html"));
+    }
+
+    @Test
+    @Order(5)
+    @WithUserDetails("Test Expenses Manager User1")
+    public void testShowAllUserDepositsForCurrentMonth()
+            throws Exception {
+        mockMvc.perform(get("/user-deposits-current-month"))
+                //.andDo(print())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentType("text/html;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeExists("deposits"))
+                .andExpect(model().attributeExists("year"))
+                .andExpect(model().attributeExists("month"))
+                .andExpect(model().attributeExists("user"))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Date")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Account")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Amount")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Source")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Income Source 1")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Bank Account 1")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("200.0")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Expenses Manager User1")))
+                .andExpect(view().name(
+                        "transactions/user-deposits-by-month.html"));
+    }
+
+    @Test
+    @Order(3)
+    @WithUserDetails("Test Expenses Manager User1")
+    public void testShowMakeDepositFormPage() throws Exception {
+        mockMvc
+                .perform(get("/make-deposit"))
+                //.andDo(print())
+                .andExpect(MockMvcResultMatchers.content()
+                        .contentType("text/html;charset=UTF-8"))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().attributeExists("savingsAccounts"))
+                .andExpect(model().attributeExists("incomeSources"))
+                .andExpect(model().attributeExists("deposit"))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Make Deposit")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Date")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Amount")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Savings Account")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Income Source")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Income Source 1")))
+                .andExpect(MockMvcResultMatchers.content().string(
+                        containsString("Test Bank Account 1")))
+                .andExpect(view().name(
+                        "transactions/make-deposit.html"));
     }
 
 
